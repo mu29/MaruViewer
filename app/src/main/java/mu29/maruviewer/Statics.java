713b1961d0d3aa216c03436ec3583ee3f83a921b
InJung Chung <mu29gl@gmail.com>
2015-07-09 12:36:14 +0900

    add statics class

---
+++ app/src/main/java/mu29/maruviewer/Statics.java
@@ -0,0 +1,17 @@
+package mu29.maruviewer;
+
+/**
+ * Created by InJung on 2015. 7. 8..
+ */
+public class Statics {
+    public static class Url {
+        public static final String UPDATE = "http://marumaru.in/?c=26&p=";
+        public static final String LIST = "http://marumaru.in/?c=40&p=";
+    }
+
+    public static class Task {
+        public static final int UPDATE = 0;
+        public static final int LIST = 1;
+        public static final int BOOKMARK = 2;
+    }
+}
