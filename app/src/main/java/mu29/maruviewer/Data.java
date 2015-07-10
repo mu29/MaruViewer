335d15bfd34eecaa879f9852d40887ac52c6401f
InJung Chung <mu29gl@gmail.com>
2015-07-10 19:22:48 +0900

    find comic function

--- app/src/main/java/mu29/maruviewer/Data.java
+++ app/src/main/java/mu29/maruviewer/Data.java
@@ -9,6 +9,15 @@ public class Data {
     public static ArrayList<ComicInfo> comics = new ArrayList<>();
     public static int downloadStatus = 0;
 
+    public static ComicInfo findComic(String title) {
+        for (ComicInfo info : comics) {
+            if (info.getTitle().equals(title))
+                return info;
+        }
+
+        return null;
+    }
+
     public static class Url {
         public static final String BASE = "http://marumaru.in";
         public static final String UPDATE = "http://marumaru.in/?c=26&p=";
